308fa00bd744caa6ffc5a1ba8ad5ba22ca996711
oushiming <oushiming@sxt.com>
2018-08-10 21:46:24 +0800

    快速开始

---
+++ src/main/java/com/hmily/rabbitmqapidemo/common/RabbitMQConfig.java
@@ -0,0 +1,9 @@
+package com.hmily.rabbitmqapidemo.common;
+
+public interface RabbitMQConfig {
+
+    final static String RABBITMQ_HOST = "192.168.0.7";
+    final static int RABBITMQ_PORT = 5672;
+    final static String RABBITMQ_DEFAULT_VIRTUAL_HOST = "/";
+
+}
--- src/main/java/com/hmily/rabbitmqapidemo/quickstart/Consumer.java
+++ src/main/java/com/hmily/rabbitmqapidemo/quickstart/Consumer.java
@@ -1,10 +1,41 @@
 package com.hmily.rabbitmqapidemo.quickstart;
 
+import com.hmily.rabbitmqapidemo.common.RabbitMQConfig;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
 import lombok.extern.slf4j.Slf4j;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 /**
  * 快速开始：消费者
  */
 @Slf4j
 public class Consumer {
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(RabbitMQConfig.RABBITMQ_HOST);
+        connectionFactory.setPort(RabbitMQConfig.RABBITMQ_PORT);
+        connectionFactory.setVirtualHost(RabbitMQConfig.RABBITMQ_DEFAULT_VIRTUAL_HOST);
+
+        Connection connection = connectionFactory.newConnection();
+
+        Channel channel = connection.createChannel();
+
+        String queueName = "test1001";
+        channel.queueDeclare(queueName, true, false, false, null);
+
+        QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+        channel.basicConsume(queueName, true, queueingConsumer);
+        log.info("消费端启动啦~");
+        while (true) {
+            Delivery delivery = queueingConsumer.nextDelivery();
+            String msg = new String(delivery.getBody());
+            log.info("消费端接收到：{}", msg);
+        }
+    }
 }
